e1b95c805265ec4c7db24f2d00e4cebd1275bdc3
Rioja97 <140468397+Rioja97@users.noreply.github.com>
2025-06-23 22:09:54 -0300

    Delete TorneosFutbol-main/TorneosFutbol-main/GestionTorneos directory

--- TorneosFutbol-main/TorneosFutbol-main/GestionTorneos/src/main/java/com/example/GestionTorneos/repository/TorneoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.GestionTorneos.repository;
-
-import com.example.GestionTorneos.model.Torneo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TorneoRepository extends JpaRepository<Torneo, Long> {
-}
